dec99af15d9379c469c6085c57cbf28efb2b1f49
Renegatto <edittorr@mail.ru>
2025-01-29 02:02:38 +0300

    Fix formatting, remove side effect

--- src/app/MyComponent.tsx
+++ src/app/MyComponent.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { ElementType, FC, ReactElement, ReactNode, useRef, useState } from "react";
+import { ElementType, FC, ReactElement, ReactNode, useEffect, useRef, useState } from "react";
 
 type SizePx = { x: number, y: number }
 type Color = { r: number, g: number, b: number }
@@ -16,7 +16,7 @@ const colorToCode = ({r,g,b}: Color): string => {
 
 type OngoingGameStateAlg<A> = {
   playing: A,
-  ended(failed: boolean): A,
+  ended(failed: boolean, difference: number): A,
 }
 type OngoingGameState = <A>(alg: OngoingGameStateAlg<A>) => A
 
@@ -29,36 +29,72 @@ const randomColor = (): Color => {
   }
 }
 
-export const Game: FC<{sizePx: SizePx}> = ({sizePx}) => {
-  const [guessedColor, setGuessedColor] = useState(() => randomColor())
+export type GameProps = {
+  sizePx: SizePx,
+  maxDifferenceToWin: number,
+}
+
+export const Game: FC<GameProps> = ({sizePx, maxDifferenceToWin}) => {
+  // it can not be defined initially since the only way to obtain it is to
+  // perform side-effect
+  const [guessedColor, setGuessedColor] = useState<Color | undefined>()
+  useEffect(
+    () => setGuessedColor(randomColor())
+    ,[]
+  )
+
   const [gameId,setGameId] = useState(0)
   const restartGame = (): void => {
     setGuessedColor(randomColor())
     setGameId(id => (id + 1) % 2)
   }
   return <>
-    <GameRound key={gameId} sizePx={sizePx} actualColor={guessedColor} restartGame={restartGame}/>
+    { guessedColor &&
+      <GameRound
+        key={gameId}
+        sizePx={sizePx}
+        actualColor={guessedColor}
+        restartGame={restartGame}
+        maxDifferenceToWin={maxDifferenceToWin}
+      />
+    }
   </>
 }
 
-const closeTo = (color1: Color, color2: Color): boolean => null as any
+const closeTo = (maxDistance: number, color1: Color, color2: Color): [boolean, number] => {
+  const sq = (a: number, b: number): number => Math.pow(a - b,2);
+  const distance = Math.sqrt(
+    sq(color1.r, color2.r)
+    + sq(color1.g, color2.g)
+    + sq(color1.b, color2.g)
+  )
+  return [maxDistance >= distance, distance]
+}
 
-export const GameRound: FC<{sizePx: SizePx, actualColor: Color, restartGame: () => void}> = ({
-  sizePx,
-  restartGame,
-  actualColor
-}) => {
+export const GameRound: FC<{
+  sizePx: SizePx,
+  actualColor: Color,
+  restartGame: () => void,
+  maxDifferenceToWin: number,
+}> = ({sizePx, restartGame, actualColor, maxDifferenceToWin}) => {
   const [gameState,setGameState] =
     useState<OngoingGameState>(() => <A,>(alg: OngoingGameStateAlg<A>): A => alg.playing)
   const [pickedColor,setPickedColor] = useState<Color>(
     {r:0x88,g:0x88,b:0x88}
   )
   const onPickColor = (): void => {
-    if (closeTo(pickedColor,actualColor)) {
-      setGameState((_: OngoingGameState) => <A,>(alg: OngoingGameStateAlg<A>): A => alg.ended(false))
-    } else {
-      setGameState((_: OngoingGameState) => <A,>(alg: OngoingGameStateAlg<A>): A => alg.ended(true))
-    }
+    const [matches, difference] = closeTo(
+      maxDifferenceToWin,
+      pickedColor,
+      actualColor
+    )
+    setGameState((_: OngoingGameState) => <A,>(alg: OngoingGameStateAlg<A>): A =>
+        alg.ended(!matches, difference)
+    )
+    // } else {
+    //   setGameState((_: OngoingGameState) => <A,>(alg: OngoingGameStateAlg<A>): A =>
+    //     alg.ended(true, difference))
+    // }
   }
   return <div>
     <ColorPicker
@@ -75,8 +111,8 @@ export const GameRound: FC<{sizePx: SizePx, actualColor: Color, restartGame: () 
 
         }} onClick={onPickColor}>Pick</button>
       </>,
-      ended: failed => <>
-        {failed ? "Wrong!" : "Correct!"}<br/>
+      ended: (failed,difference) => <>
+        {failed ? "Wrong!" : "Correct!"} difference is {Math.round(difference)}<br/>
         <ColorsComparison color={actualColor} color2={pickedColor} sizePx={sizePx}/>
         <button type="button" style={{
           border:"2px solid black",
@@ -98,7 +134,6 @@ export const ColoredBackground: FC<Props> = ({ color, sizePx }: Props) =>
       width: `${sizePx.x}px`,
       height: `${sizePx.y}px`,
       position: "relative",
-      //order:"2px solid black",
       backgroundColor: colorToCode(color),
     }}> Color {colorToCode(color)}
     </div>
@@ -109,19 +144,41 @@ const ColorPicker: FC<{disabledWith?: Color, update: (color: Color) => void}> = 
   const [g,setG] = useState(0)
   const [b,setB] = useState(0)
   const currentColor: Color = {r,g,b}
+
   const disabled = disabledWith != undefined
+
   const drawGhostSlider = (pickColor: (color: Color) => number): ReactElement =>
-    whenDisabled(actual => GhostSlider(actual.r))
+    <GhostSlider value={disabledWith === undefined ? undefined : pickColor(disabledWith)}/>
+  const drawColorSlider = (setColor: (color: number) => void): ReactElement =>
+    <ColorSlider disabled={disabled} onChange={c => { setColor(c); update(currentColor) }}/>
+
+  const drawSlidersPair = (
+    updateColor: (color: number) => void,
+    colorOf: (color: Color) => number,
+    colorName: string,
+  ): ReactElement =>
+    <>
+      {`${colorName}: `} 
+      {drawColorSlider(updateColor)}
+      {drawGhostSlider(colorOf)}
+    </>
+
   const whenDisabled = (child: (actualColor: Color) => ReactElement): ReactElement =>
     disabledWith === undefined ? <></> : child(disabledWith)
+
+  const overlayOnDisabled =
+    <div style={{
+      backgroundColor: "#88888888",
+      width:"100%",
+      height: "100%",
+      position: "absolute",
+    }}/>
+
   return <div style={{width: "100%", height: "100%", overflow: "hidden", position: "relative"}}>
-    {whenDisabled(_ => <div style={{backgroundColor: "#88888888", width:"100%", height: "100%" , position: "absolute"}}/>)}
-    R: <ColorSlider disabled={disabled} onChange={r => { setR(r); update(currentColor) }}/>
-    {drawGhostSlider(color => color.r)}
-    G: <ColorSlider disabled={disabled} onChange={g => { setG(g); update(currentColor) }}/>
-    {drawGhostSlider(color => color.g)}
-    B: <ColorSlider disabled={disabled} onChange={b => { setB(b); update(currentColor) }}/>
-    {drawGhostSlider(color => color.b)}
+    {whenDisabled(_ => overlayOnDisabled)}
+    {drawSlidersPair(setR, color => color.r, "R")}
+    {drawSlidersPair(setG, color => color.g, "G")} 
+    {drawSlidersPair(setB, color => color.b, "B")} 
   </div>
 }
 
@@ -142,12 +199,13 @@ const useDebounce = (delayMs: number): ((cont: () => void) => void) => {
 }
 
 
-const GhostSlider = (value: number): ReactElement => {
-  return <>
+const GhostSlider: FC<{value?: number}> = ({value}) =>
+  <>
     <div className="slidecontainer" style={{width: "100%", height: "100%", overflow: "hidden", position: "relative"}}>
       <input
         type="range"
         disabled={true}
+        style={{visibility: value === undefined ? "hidden" : undefined}}
         min="0"
         max="255"
         value={value}
@@ -155,7 +213,7 @@ const GhostSlider = (value: number): ReactElement => {
       />
     </div>
   </>
-}
+
 const ColorSlider: FC<{disabled: boolean, onChange: (val: number) => void}> = ({disabled,onChange}) => {
   const [value,setValue] = useState(0)
   const debounce = useDebounce(10)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           priority
         />
       </div> */}
-      <Game sizePx={{x:400,y:200}}/>
+      <Game sizePx={{x:400,y:200}} maxDifferenceToWin={30}/>
       {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"

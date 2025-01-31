749a1e7de46e1dc4dddbc43ffdefbc9d52a31715
Renegatto <edittorr@mail.ru>
2025-01-31 15:54:37 +0300

    Abstract main game logic to tagless final

--- src/app/Game.tsx
+++ src/app/Game.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { FC, ReactElement, useEffect, useState } from "react";
+import { CSSProperties, FC, ReactElement, useEffect, useState } from "react";
 import { useDebounce } from "./Hooks";
 import { Color, Option, colorToCode, Current, eachIsClose, State, randomColor, Lens, SimpleLens, Some, None } from "./Utils";
 
@@ -47,6 +47,38 @@ type GameState =
   & PickedColorState
   & GameStateState
 
+// Algebras
+
+type Div<in out A> = {
+  div(
+    className: string,
+    style: CSSProperties,
+    hidden?: boolean,
+  ): (childs: A[]) => A,
+}
+type Str<in out A> = {
+  str(text: string): A,
+}
+type Empty<in out A> = {
+  empty: A,
+}
+type Fold<in out A> = {
+  fold: (elems: A[]) => A,
+}
+type DifficultyPicker<in out A> = {
+  difficultyPicker: (state: DifficultyState) => A,
+}
+type Buttons<in out A> = {
+  restartBtn: (restartGame: () => void) => A,
+  pickColorBtn: (onPickColor: () => void) => A,
+}
+type Sliders<in out A> = {
+  ghostSlider: (value: Option<number>) => A,
+  colorSlider: (disabled: boolean, onChange: (m: number) => void, child: A) => A,
+}
+
+// Components
+
 export const Game: FC = () => {
   // it can not be defined initially since the only way to obtain it is to
   // perform side-effect
@@ -76,10 +108,14 @@ export const Game: FC = () => {
       PickedColor: State(pickedColor, setPickedColor),
       GameState: State(gameState, setGameState),
     }
-    return <GameRound
+    return <GameRoundComponent
       key={gameId}
       state={state}
-      restartGame={restartGame}
+      restartGame={() => {
+        restartGame();
+        // just for fun, let's print the schema
+        console.log(GameRoundSchema({state,restartGame}));
+      }}
     />
   }
 }
@@ -91,7 +127,7 @@ type GameRoundProps = {
   & PickedColorState
   & Current<GuessedColorState>
 }
-export const GameRound: FC<GameRoundProps> = ({
+export const GameRound = ({
   restartGame,
   state: {
     Difficulty,
@@ -99,7 +135,10 @@ export const GameRound: FC<GameRoundProps> = ({
     PickedColor,
     GuessedColor: { current: actualColor },
   },
-}) => {
+}: GameRoundProps) =>
+<A,>(
+  alg: Div<A> & Empty<A> & Str<A> & Fold<A> & Sliders<A> & Buttons<A> & DifficultyPicker<A>,
+): A => {
   const onPickColor = (): void => {
     const [matches, maxDifference] = eachIsClose(
       Difficulty.current,
@@ -119,33 +158,29 @@ export const GameRound: FC<GameRoundProps> = ({
     playing: true,
     ended: () => false,
   })
-  return <div className="game-round">
-    <ColorPicker
-      disabledWith={
-        GameState.current.match({
-          ended: outcome => Some({ actual: actualColor, outcome }),
-          playing: None()
-        })
-      }
-      state={{ PickedColor }}
-    />
-    <InfoBar state={{ Difficulty, GameState }} />
-    <div className="colored-background">
-      { stillPlaying
-        ? <ColoredBackground color={actualColor} child={<></>} />
-        : <ColorsComparison actual={actualColor} picked={PickedColor.current} />
-      }
-    </div>
-    <div className="game bottom-block">
-      { stillPlaying
-        ? <PickColorBtn onPickColor={onPickColor}/>
-        : <div className="game bottom-block reset-options">
-            <RestartBtn restartGame={restartGame}/>
-            <DifficultyPicker state={{ Difficulty }} />
-          </div>
-      }
-    </div>
-  </div>
+  return alg.div("game-round",{})([
+    ColorPicker(
+      GameState.current.match({
+        ended: outcome => Some({ actual: actualColor, outcome }),
+        playing: None(),
+      }),
+      { PickedColor },
+    )(alg),
+    InfoBar({ Difficulty, GameState })(alg),
+    alg.div("colored-background",{})([
+      stillPlaying
+        ? ColoredBackground(actualColor,alg.empty)(alg)
+        : ColorsComparison(actualColor,PickedColor.current)(alg)
+    ]),
+    alg.div("game bottom-block",{})([
+      stillPlaying
+        ? alg.pickColorBtn(onPickColor)
+        : alg.div("game bottom-block reset-options",{})([
+            alg.restartBtn(restartGame),
+            alg.difficultyPicker({Difficulty}),
+          ])
+    ]),
+  ]);
 }
 
 const PickColorBtn: FC<{ onPickColor: () => void }> = ({onPickColor}) =>
@@ -166,126 +201,118 @@ const RestartBtn: FC<{ restartGame: () => void }> = ({restartGame}) =>
     Restart
   </button>
 
-export const InfoBar: FC<{
-  state: Current<DifficultyState> & Current<GameStateState>
-}> =
-  ({ state: { Difficulty, GameState } }) => {
-    return <div className="info-bar">
-      {GameState.current.match({
-        playing:
-          <div style={{ visibility: "hidden" }}></div>,
-        ended: (outcome, difference, _) =>
-          <div>
-            {outcome.match({
-              victory: "Great job!",
-              defeat: "Wrong!",
-            })} Difference is {Math.round(difference)}
-          </div>,
-      })}
-      <div>
-        Difficulty: {
-          displayDifficulty(GameState.current.match({
-            playing: Difficulty.current,
-            // we only display difficulty AT THE MOMENT game was over
-            ended: (_out, _diff, difficulty) => difficulty,
-          }))
-        }
-      </div>
-    </div>
-  }
+export const InfoBar =
+  ({ Difficulty, GameState } :
+    Current<DifficultyState> & Current<GameStateState>
+  ) =>
+  <A,>(alg: Div<A> & Str<A>): A =>
+  alg.div("info-bar",{})([
+    GameState.current.match({
+      playing: alg.div("",{},true)([]),
+      ended: (outcome, difference, _) =>
+        alg.div("",{})([
+          alg.str(outcome.match({
+            victory: "Great job!",
+            defeat: "Wrong!",
+          })),
+          alg.str(`Difference is ${Math.round(difference)}`),
+        ]),
+    }),
+    alg.div("",{})([
+      alg.str(`Difficulty: ${
+        displayDifficulty(GameState.current.match({
+          playing: Difficulty.current,
+          // we only display difficulty AT THE MOMENT game was over
+          ended: (_out, _diff, difficulty) => difficulty,
+        }))
+      }`),
+    ]),
+  ])
 
-export const ColorsComparison: FC<{ actual: Color, picked: Color }> =
-  ({ actual, picked }) =>
-    <>
-      <div className="colored-background comparison">
-        <ColoredBackground
-          color={actual}
-          child={<>Actual color {colorToCode(actual)}</>}
-        />
-      </div>
-      <div className="colored-background comparison">
-        <ColoredBackground
-          color={picked}
-          child={<>Your color {colorToCode(picked)}</>}
-        />
-      </div>
-    </>
+export const ColorsComparison =
+  (actual: Color, picked: Color) =>
+  <A,>(alg: Div<A> & Str<A> & Fold<A>): A =>
+  alg.fold([
+    alg.div("colored-background comparison",{})([
+      ColoredBackground(actual,alg.str(`Actual color ${colorToCode(actual)}`))(alg),
+    ]),
+    alg.div("colored-background comparison",{})([
+      ColoredBackground(
+        picked,
+        alg.str(`Your color ${colorToCode(picked)}`),
+      )(alg),
+    ]),
+  ])
 
-export const ColoredBackground: FC<{ color: Color, child: ReactElement }> =
-  ({ color, child }) =>
-    <>
-      <div className="colored-background background" style={{
-        backgroundColor: colorToCode(color),
-      }}>
-        {child}
-      </div>
-    </>
-
-type ColorPickerProps = {
-  disabledWith: Option<{ actual: Color, outcome: Outcome }>,
-  state: PickedColorState,
-}
-
+export const ColoredBackground =
+  <A,>(color: Color, child: A) =>
+  (alg: Div<A>): A =>
+  alg.div("colored-background background", {
+    backgroundColor: colorToCode(color),
+  })
+  ([child])
+  
 const withR: SimpleLens<Color, number> = Lens.property("r")
 const withG: SimpleLens<Color, number> = Lens.property("g")
 const withB: SimpleLens<Color, number> = Lens.property("b")
 
-const ColorPicker: FC<ColorPickerProps> =
-  ({ disabledWith, state: { PickedColor } }) => {
+const ColorPicker = (
+    disabledWith: Option<{ actual: Color, outcome: Outcome }>,
+    { PickedColor }: PickedColorState
+  ) =>
+  <A,>(alg: Div<A> & Str<A> & Empty<A> & Fold<A> & Sliders<A>): A => {
+  const currentColor: Color = PickedColor.current
 
-    const currentColor: Color = PickedColor.current
+  const disabled = disabledWith.match({
+    some: () => true,
+    none: false
+  })
+  const drawGhostSlider = (component: SimpleLens<Color, number>): A =>
+    alg.ghostSlider(disabledWith.match({
+      none: None(),
+      some: ({ actual }) => Some(component.get(actual)),
+    }))
+  const drawColorSlider = (
+    component: SimpleLens<Color, number>,
+    colorName: string,
+  ): A =>
+    alg.colorSlider(
+      disabled,
+      c =>
+        PickedColor.update(() =>
+          component.modify(currentColor, c)
+        ),
+      alg.str(`${colorName}: ${component.get(currentColor)}`),
+    );
 
-    const disabled = disabledWith.match({
-      some: () => true,
-      none: false
+  const drawSlidersPair = (
+    component: SimpleLens<Color, number>,
+    colorName: string,
+  ): A =>
+    alg.fold([
+      drawColorSlider(component, colorName),
+      drawGhostSlider(component),
+    ]);
+
+  const whenDisabled = (child: (subclass: string) => A): A =>
+    disabledWith.match({
+      some: ({ outcome }) =>
+        child(outcome.match({
+          victory: "overlay-on-victory",
+          defeat: "overlay-on-defeat",
+        })),
+      none: alg.empty,
     })
-    const drawGhostSlider = (component: SimpleLens<Color, number>): ReactElement =>
-      <GhostSlider value={disabledWith.match({
-        none: None(),
-        some: ({ actual }) => Some(component.get(actual)),
-      })} />
-    const drawColorSlider = (
-      component: SimpleLens<Color, number>,
-      colorName: string,
-    ): ReactElement =>
-      <ColorSlider
-        disabled={disabled}
-        onChange={c =>
-          PickedColor.update(() =>
-            component.modify(currentColor, c)
-          )
-        }
-        child={<>{colorName}: {component.get(currentColor)}</>}
-      />
+  const overlayOnDisabled = (subclass: string) =>
+    alg.div(`color-picker ${subclass}`,{})([])
 
-    const drawSlidersPair = (
-      component: SimpleLens<Color, number>,
-      colorName: string,
-    ): ReactElement =>
-      <>
-        {drawColorSlider(component, colorName)}
-        {drawGhostSlider(component)}
-      </>
-
-    const whenDisabled = (child: (subclass: string) => ReactElement): ReactElement =>
-      disabledWith.match({
-        some: ({ outcome }) =>
-          child(outcome.match({
-            victory: "overlay-on-victory",
-            defeat: "overlay-on-defeat",
-          })),
-        none: <></>,
-      })
-    const overlayOnDisabled = (subclass: string) =>
-      <div className={`color-picker ${subclass}`} />
-
-    return <div className={"color-picker"}>
-      {drawSlidersPair(withR, "R")}
-      {drawSlidersPair(withG, "G")}
-      {drawSlidersPair(withB, "B")}
-      {whenDisabled(overlayOnDisabled)}
-    </div>
-  }
+  return alg.div("color-picker",{})([
+    drawSlidersPair(withR, "R"),
+    drawSlidersPair(withG, "G"),
+    drawSlidersPair(withB, "B"),
+    whenDisabled(overlayOnDisabled),
+  ])
+}
 
 const GhostSlider: FC<{ value: Option<number> }> = ({ value }) =>
   <>
@@ -369,3 +396,91 @@ const DifficultyPicker: FC<{ state: DifficultyState }> =
       </div>
     </>
   }
+
+namespace Algebras {
+  export const divElement: Div<ReactElement> = {
+    div: (
+      className,
+      style,
+      hidden,
+    ) => childs =>
+    <div className={className} style={style} hidden={hidden}>
+      { foldElements.fold(childs)
+      }
+    </div>,
+  }
+  export const strElement: Str<ReactElement> = {
+    str: text => <>{text}</>,
+  }
+  export const emptyElement: Empty<ReactElement> ={
+    empty: <></>,
+  }
+  export const foldElements: Fold<ReactElement> = {
+    fold: childs => childs.reduce(
+      (acc,x) => <>{acc}{x}</>,
+      <></>,
+      ),
+  }
+  export const difficultyPickerElement: DifficultyPicker<ReactElement> = {
+    difficultyPicker: state => <DifficultyPicker state={state}/>,
+  }
+  export const buttonsElements: Buttons<ReactElement> = {
+    restartBtn: restartGame => <RestartBtn restartGame={restartGame}/>,
+    pickColorBtn: onPickColor => <PickColorBtn onPickColor={onPickColor}/>,
+  }
+  export const sliderElements: Sliders<ReactElement> = {
+    ghostSlider: value => <GhostSlider value={value}/>,
+    colorSlider: (disabled, onChange, child) =>
+      <ColorSlider disabled={disabled} onChange={onChange} child={child}/>,
+  }
+
+  const foldText = (xs: string[]): string =>
+    xs.reduce((a,b) => `${a}${b}`,'')
+  export const stringSchema:
+    Div<string>
+    & Str<string>
+    & Empty<string>
+    & Fold<string>
+    & Buttons<string>
+    & DifficultyPicker<string>
+    & Sliders<string> = {
+    div: (className,style,hidden) => childs =>
+      `[div class='${
+          className
+        }' style=${
+          JSON.stringify(style)
+        } ${
+          hidden === undefined ? 'hidden' : ''
+        }]${
+        foldText(childs)
+      }[end div]`,
+    str: text => `'${text}'`,
+    empty: '',
+    fold: foldText,
+    restartBtn: () => '[RestartBtn]',
+    difficultyPicker: (difficultyState) =>
+      `[DifficultyPicker difficulty=${difficultyState.Difficulty.current}]`,
+    pickColorBtn: () => '[PickColorBtn]',
+    ghostSlider: (disabledWith: Option<number>) =>
+      disabledWith.match({
+        some: n => `[GhostSlider n=${n}]`,
+        none: '[GhostSlider]'
+      }),
+    colorSlider: (disabled, _, child) =>
+      `[ColorSlider ${disabled ? 'disabled' : ''}]${child}[end ColorSlider]`,
+  }
+}
+
+const GameRoundComponent: FC<GameRoundProps> = (props) =>
+  GameRound(props)({
+    ...Algebras.divElement,
+    ...Algebras.strElement,
+    ...Algebras.emptyElement,
+    ...Algebras.foldElements,
+    ...Algebras.difficultyPickerElement,
+    ...Algebras.buttonsElements,
+    ...Algebras.sliderElements,
+  })
+
+const GameRoundSchema = (props: GameRoundProps): string =>
+  GameRound(props)(Algebras.stringSchema)
